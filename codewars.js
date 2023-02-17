047be365b3ee81ea0e7feb59619cc72fb6a3023a
AlexandrSevastyuk <104134957+AlexandrSevastyuk@users.noreply.github.com>
2023-02-17 13:42:22 +0300

    BigInt

--- codewars.js
+++ codewars.js
@@ -51,3 +51,24 @@ function comp(array1, array2){
        return false
      }
    }
+
+//BigINt
+
+function res(a, b, result, carry, base) {
+    if(a.length == 0 && b.length == 0 && !carry) {
+     if (result[0]=='0') {
+        return result.substring(1);} else {
+            return result;
+        }
+    }
+    var left = parseInt(a.pop() || '0', 10);
+    var right = parseInt(b.pop() || '0', 10);
+  
+    
+    var l = left + right + (carry || 0);
+   
+    return res(a, b, l % base + (result || ""), Math.floor(l/base), base);
+  }
+  
+ function add(a, b) {
+    return res(a.split(""), b.split(""), "","",10).toString();}
